34f4a352f453a9decb85533d2bdbb8363a8a2e75
Unknown <songshan_you@mymail.sutd.edu.sg>
2018-06-14 16:39:13 +0800

    Added button.cpp to replace serialButton.py

---
+++ button.cpp
@@ -0,0 +1,31 @@
+#include <wiringPi.h>
+#include <string.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    int LED_PIN = 7 ;
+    int BTN_PIN_1 = 12 ;
+    int btn_state_1 = 0;
+    char msg ;
+
+    wiringPiSetupPhys() ;
+    pinMode (LED_PIN, OUTPUT) ;
+    pinMode (BTN_PIN_1, INPUT) ;
+    for(;;)
+    {
+        btn_state_1 = digitalRead(BTN_PIN_1);
+        if(btn_state_1 == 1){
+            msg = 0;
+            digitalWrite(LED_PIN, HIGH) ;
+        }
+        else{
+            msg = 1;
+            digitalWrite(LED_PIN, LOW) ;
+        }
+        printf("%d\n",msg);
+        delay (100) ;
+  }
+}
+
