53c4bff58b48e3a78ded6ea70c8b42e3d6b5931c
Unknown <songshan_you@mymail.sutd.edu.sg>
2018-06-20 17:05:27 +0800

    Upgraded to two buttons

--- one_button.py
+++ one_button.py
@@ -9,53 +9,65 @@ from std_msgs.msg import String
 from rmf_msgs.msg import CallButtonState
 
 # GPIO declaration
-LED_PIN = 7
-BTN_PIN_1 = 12
-# BTN_PIN_2 = 16
-# BTN_PIN_3 = 18
+LED_PIN_0 = 7
+LED_PIN_1 = 11
+BTN_PIN_0 = 12
+BTN_PIN_1 = 16
+# BTN_PIN_2 = 18
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(LED_PIN,GPIO.OUT)
+GPIO.setup(LED_PIN_0,GPIO.OUT)
+GPIO.setup(LED_PIN_1,GPIO.OUT)
+GPIO.setup(BTN_PIN_0,GPIO.IN)
 GPIO.setup(BTN_PIN_1,GPIO.IN)
 # GPIO.setup(BTN_PIN_2,GPIO.IN)
-# GPIO.setup(BTN_PIN_3,GPIO.IN)
 
+btn_state_0 = False
 btn_state_1 = False
 # btn_state_2 = False
-# btn_state_3 = False
 bed_dict = {12:"bed_0_button", 16:"bed_1_button", 18:"bed_2_button"}
-
-# def led_update():
-#     # if btn_state_1 == True or btn_state_2 == True or btn_state_3 == True:
-#     if btn_state_1 == True:
-#         GPIO.output(LED_PIN,GPIO.HIGH)
-#     else:
-#         GPIO.output(LED_PIN,GPIO.LOW)
 
 def main(args=None):
     # ROS init
     rclpy.init(args=args)
     node = rclpy.create_node('button_node')
     publisher = node.create_publisher(CallButtonState, 'call_button_state')
-    msg = CallButtonState()
+    msg_0 = CallButtonState()
+    msg_1 = CallButtonState()
 
-    while rclpy.ok():
-        btn_state_1 = GPIO.input(BTN_PIN_1)
-        # btn_state_2 = GPIO.input(BTN_PIN_2)
-        # btn_state_3 = GPIO.input(BTN_PIN_3)
+    try:
 
-        msg.id = bed_dict[BTN_PIN_1]
-        msg.pressed = bool(btn_state_1)
-        print("msg: " + msg.id + ", " +  str(msg.pressed))
-        node.get_logger().info("%s,%s" % (msg.id,msg.pressed))
-        publisher.publish(msg)
-        if btn_state_1 == True:
-            GPIO.output(LED_PIN,GPIO.HIGH)
-        else:
-            GPIO.output(LED_PIN,GPIO.LOW)
-        time.sleep(0.1)
+        while rclpy.ok():
+            btn_state_0 = GPIO.input(BTN_PIN_0)
+            btn_state_1 = GPIO.input(BTN_PIN_1)
+            # btn_state_2 = GPIO.input(BTN_PIN_2)
 
-    node.destroy_node()
-    rclpy.shutdown()
+            # Update & publish msg
+            msg_0.id = bed_dict[BTN_PIN_0]
+            msg_0.pressed = bool(btn_state_0)
+            msg_1.id = bed_dict[BTN_PIN_1]
+            msg_1.pressed = bool(btn_state_1)
+            node.get_logger().info("%s,%s" % (msg_0.id,msg_0.pressed))
+            node.get_logger().info("%s,%s" % (msg_1.id,msg_1.pressed))
+            publisher.publish(msg_0)
+            publisher.publish(msg_1)
+
+            # Update LEDs
+            if btn_state_0 == True:
+                GPIO.output(LED_PIN_0,GPIO.HIGH)
+            else:
+                GPIO.output(LED_PIN_0,GPIO.LOW)
+            if btn_state_1 == True:
+                GPIO.output(LED_PIN_1,GPIO.HIGH)
+            else:
+                GPIO.output(LED_PIN_1,GPIO.LOW)
+
+            time.sleep(0.2)
+            
+    except KeyboardInterrupt:
+        node.destroy_node()
+        rclpy.shutdown()
+        GPIO.cleanup()
+        print("Cleaned up!")
 
 if __name__ == '__main__':
     main()
