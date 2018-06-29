f0a42fd65a3bd65f93912d6737c0a552c81c62e7
Unknown <songshan_you@mymail.sutd.edu.sg>
2018-06-29 15:21:03 +0800

    Updated to 16 buttons

--- button_pub.py
+++ button_pub.py
@@ -3,27 +3,42 @@ import time
 
 # ROS library
 import rclpy
-from std_msgs.msg import String
+from rmf_msgs.msg import CallButtonState
 
 port = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=3.0)
+state_dict = {}
+bool_state = {False:0, True:1}
 
 def main(args=None):
     # ROS init
     rclpy.init(args=args)
     node = rclpy.create_node('button_node')
-    publisher = node.create_publisher(String, 'button')
-    msg = String()    
+    publisher = node.create_publisher(String, 'call_button_state')
+    
+    for i in range(16):
+        # set all button state to False
+        state_dict[i] = False
 
-    while rclpy.ok():
-        rcv = port.readline()
-        print("Read: " + repr(rcv)[2:-3])
-        msg.data = repr(rcv)[2:-3]
-        node.get_logger().info("%s" % msg.data)
-        publisher.publish(msg)
-        time.sleep(0.1)
+    try:
+        while rclpy.ok():
+            # Read serial msg from Arduino
+            serial = port.readline()
+            print("Read: " + repr(serial))
 
-    node.destroy_node()
-    rclpy.shutdown()
+            for i in range(len(serial)):
+                if serial[i] != bool_state[state_dict[i]]: # button state change
+                    # Update msg
+                    msg.id = "bed_" + str(i) + "_button"
+                    msg.pressed = bool(state_dict[i])
+                    # Log & publish msg
+                    node.get_logger().info("%s,%s" % (msg.id,msg.pressed))
+                    publisher.publish(msg)
+                    
+            time.sleep(0.1)
+            
+    except KeyboardInterrupt:
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
